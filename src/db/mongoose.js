135c13aefb7da0573d3d85305355fc0a592b25c5
Michael Houmann <mhoumann87@gmail.com>
2021-06-20 19:01:21 +0200

    Add connection to database and model for Task :card_file_box:

---
+++ src/db/mongoose.js
@@ -0,0 +1,13 @@
+// Get mongoose in the project
+const mongoose = require('mongoose');
+
+// Set up connection variables
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'task-manager-api';
+
+// Connect to the database
+mongoose.connect(`${connectionURL}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
---
+++ src/models/task.js
@@ -0,0 +1,16 @@
+// Import dependencies
+const mongoose = require('mongoose');
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = Task;
