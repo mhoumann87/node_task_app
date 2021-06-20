3d2166dc60caa624ddb75eb9d448591e973d3360
Michael Houmann <mhoumann87@gmail.com>
2021-06-20 18:36:54 +0200

    Add model for User :card_file_box:

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+// Import dependencies
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password can\'t contain the word "password"');
+      }
+    },
+  },
+});
+
+module.exports = User;
